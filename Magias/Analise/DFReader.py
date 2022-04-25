68864ea1db8c1715a4bae88e159406d931ab486a
Lucas Paiolla <lucaspaiolla@gmail.com>
2022-04-24 23:26:28 -0300

    format file

--- Magias/Analise/DFReader.py
+++ Magias/Analise/DFReader.py
@@ -5,25 +5,22 @@ from typing import List
 import pandas as pd
 
 
-def get_spells_df(
-    path_prefix: str = '../',
-    sort_by: List[str]  = None
-):
+def get_spells_df(path_prefix: str = "../", sort_by: List[str] = None):
     """This function returns the spells DataFrame from the .json files.
 
     You might specify the path to the json files, the default is '../'.
     """
-    files = glob.glob(f'{path_prefix}*.json')
-    files = list(map(lambda x: x[len(path_prefix):], files))
-    files.remove('_Template.json')
+    files = glob.glob(f"{path_prefix}*.json")
+    files = list(map(lambda x: x[len(path_prefix) :], files))
+    files.remove("_Template.json")
 
     result = list()
     for file_name in files:
-        with open(f'{path_prefix}{file_name}', 'r') as file:
+        with open(f"{path_prefix}{file_name}", "r") as file:
             result.append(json.load(file))
 
     if sort_by is None:
-        sort_by = ['name', 'nivel']
+        sort_by = ["name", "nivel"]
 
     result = pd.DataFrame(result).sort_values(by=sort_by).reset_index(drop=True)
     return result
